b2ebd90a8ab157d947e400ce5a0bbf75854e8fe2
TheKoopa <TheKoppaFAF@gmail.com>
2016-07-28 13:14:12 -0400

    Add files via upload

---
+++ DefenseOfCapella/defenseofcapella_scenario.lua
@@ -0,0 +1,35 @@
+version = 3
+ScenarioInfo = {
+    name = 'DefenseOfCapella',
+    description = '',
+    type = 'campaign_coop',
+    starts = true,
+    preview = '',
+    size = {512, 512},
+    map = '/maps/DefenseOfCapella/DefenseOfCapella.scmap',
+    save = '/maps/DefenseOfCapella/DefenseOfCapella_save.lua',
+    script = '/maps/DefenseOfCapella/DefenseOfCapella_script.lua',
+    norushradius = 0.000000,
+    norushoffsetX_Player = 0.000000,
+    norushoffsetY_Player = 0.000000,
+    norushoffsetX_Coop1 = 0.000000,
+    norushoffsetY_Coop1 = 0.000000,
+    norushoffsetX_Coop2 = 0.000000,
+    norushoffsetY_Coop2 = 0.000000,
+    norushoffsetX_Coop3 = 0.000000,
+    norushoffsetY_Coop3 = 0.000000,
+    norushoffsetX_UEFCivilians = 0.000000,
+    norushoffsetY_UEFCivilians = 0.000000,
+    norushoffsetX_UEFAlly = 0.000000,
+    norushoffsetY_UEFAlly = 0.000000,
+    norushoffsetX_Seraphim = 0.000000,
+    norushoffsetY_Seraphim = 0.000000,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                { name = 'FFA', armies = {'Player','Coop1','Coop2','Coop3','UEFCivilians','UEFAlly','Seraphim',} },
+            },
+            customprops = {
+            },
+        },
+    }}
